c897f479f2508c7a10e065b35a985e1c65dc4d11
CoraEpiro <123943936+CoraEpiro@users.noreply.github.com>
2023-01-30 16:12:49 +0400

    git commit -m 'update server url'

--- client/script.js
+++ client/script.js
@@ -77,7 +77,7 @@ const handleSubmit = async (e) => {
 
     //fetch data from server -> bot's response
 
-    const response = await fetch('https://codexai-wdey.onrender.com', {
+    const response = await fetch('https://codexai-wdey.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
